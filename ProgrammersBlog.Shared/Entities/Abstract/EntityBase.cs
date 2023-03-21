2c7ad801b617745a1f7fac2112688574a3f552ba
DAS-1 <DAS-1@DESKTOP-K5EIOC9>
2023-03-21 10:24:11 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase //Temel değerleri veriyoruz.
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; //Oluşturulma tarihini oluşturulduğu zamanın.
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now; //Değiştirilme tarihi değiştirildiği degere atandı.
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedByName { get;set; }
+    }
+}
---
+++ ProgrammersBlog.Shared/Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public interface IEntity //Sdece bir imza olacak. Repository pattern için kullanıcaz. Veri tabanına yazılmasını istediğimiz tabloları, biz IEntity ile imzalıyor olacaz.
+    {
+    }
+}
